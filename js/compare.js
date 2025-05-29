a601eed87f291ff1f5151de4b82223c38c32f643
Edfram-Guerra <edfram.guerra@gmail.com>
2025-05-29 00:01:39 -0300

    Começando a implementar compare.js

--- js/compare.js
+++ js/compare.js
@@ -1,52 +1,69 @@
-
 //car
 let carArr = [];
 
 class Car {
-   
-
-    constructor(nome, preco, alturaCacamba, alturaVeiculo, alturaSolo, capacidadeCarga, motor, potencia, volumeCacamba, roda, image){
-       
+    constructor(nome, preco, alturaCacamba, alturaVeiculo, alturaSolo, capacidadeCarga, motor, potencia, volumeCacamba, roda, image) {
+        this.nome = nome;
+        this.preco = preco;
+        this.alturaCacamba = alturaCacamba;
+        this.alturaVeiculo = alturaVeiculo;
+        this.alturaSolo = alturaSolo;
+        this.capacidadeCarga = capacidadeCarga;
+        this.motor = motor;
+        this.potencia = potencia;
+        this.volumeCacamba = volumeCacamba;
+        this.roda = roda;
+        this.image = image;
     }
-} 
+}
 
-// search on array if exist carClass returning 1 if not return -1
+// procura no array se existe carClass retornando 1 se sim, -1 se não
+// obter Posicao Carro
 function GetCarArrPosition(arr, carClass) {
-    for(let i = 0; i < arr.length; i++){
-        if(arr[i].nome  === carClass.nome)
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i].nome === carClass.nome)
             return i;
     }
     return -1;
 }
 
+//definir Carro Para Comparar
 function SetCarToCompare(el, carClass) {
-   
-    if(carClass instanceof Car){       
-        if(el.checked){
-                
-            
+
+    if (carClass instanceof Car) {
+        if (el.checked) {
+
+
         } else {
-          
-        } 
+
+        }
     } else {
-        throw "You need set a Car Class";
+        throw "Você precisa definir uma classe Carro";
     }
 }
 
+//mostrar Comparacao
 function ShowCompare() {
-    if(carArr.length < 2) {
-        alert("Precisa marcar 2 carros para apresentar a comparação");
+    if (carArr.length !== 2) {
+        alert("Precisa marcar exatamente 2 carros para apresentar a comparação");
         return;
+
+    } else {
+        // atualizarTabelaComparaca
+        UpdateCompareTable();
+        document.getElementById("compare").style.display = "block";
     }
 
-    UpdateCompareTable();
-    document.getElementById("compare").style.display = "block";
+
 }
 
-function HideCompare(){
-    document.getElementById("compare").style.display = "none"; 
+//esconder Comparacao
+function HideCompare() {
+    document.getElementById("compare").style.display = "none";
 }
 
+
+//atualizar Tabela Comparacao
 function UpdateCompareTable() {
-    
+
 }
